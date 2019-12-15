0afef5b8838bd189b5fda96f620645d76f6dbdb9
jyl12 <joker_l_87@yahoo.com>
2019-12-15 21:52:33 +0800

    Clean up

---
+++ src/subscriber/src/subscriber_node.cpp
@@ -0,0 +1,14 @@
+#include"ros/ros.h"
+#include "std_msgs/String.h"
+
+void chatterCallback(const std_msgs::String& msg){
+  ROS_INFO("I heard: [%s}",msg.data.c_str());
+}
+
+int main (int argc, char ** argv){
+  ros::init(argc, argv, "subscriber");
+  ros::NodeHandle nodeHandle;
+  ros::Subscriber subscriber = nodeHandle.subscribe("chatter",10,chatterCallback);
+  ros::spin();
+  return 0;
+}
